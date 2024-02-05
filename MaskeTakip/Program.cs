108ed61d2e178e9ce8c950ba39cecb128c971d13
fathf3 <fatih-348@hotmail.com>
2024-02-05 23:49:13 +0300

    Add project files.

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+
+;
+Person p1 = new Person()
+{
+	FirstName = "Fatih",
+	LastName = "Mutlu",
+	DateOfBirthYear = 1,
+	NationalIdentity = 1
+};
+
+PttManager pttManager = new PttManager(new PersonManager());
+
+pttManager.GiveMask(p1);
